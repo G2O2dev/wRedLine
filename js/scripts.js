70678ce5d4de97a3a7d410dfd7da16d439cb274c
G2O2dev <85354527+G2O2dev@users.noreply.github.com>
2022-05-07 19:55:54 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,1 @@
+function PopUpShow(){document.getElementById("popup").style.display="block"}function PopUpHide(){document.getElementById("popup").style.display="none"}$('a[href^="#"]').on("click",(function(t){var e=$(this.getAttribute("href"));e.length&&(t.preventDefault(),$("html, body").stop().animate({scrollTop:e.offset().top},1200))}));
